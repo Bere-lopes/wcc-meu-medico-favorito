515acd2260e8a98b2c0e1b53a78a630a2d9aa34a
Berenice <berenicelopesmaria@gmal.com>
2021-11-26 10:25:23 -0300

    ajustando configurações

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 app.use(function (req, res, next)  {
-    res. header("Access-Control-Allow-Origin", "*");
-    res. header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Request-With, Cont-Type, Accept"
-    );
-    next();
-});
-module.exports = app;
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Acces-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+app.options("/*", (req, res)  =>  {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers")
+  res.header(
+    "Access-Control-Allow-Methods",
+    "PUT,POST,GET,DELETE,OPTIONS,PATCH"
+  );
+  res.send("send some thing whatever")
+})
+module.exports = {
+  app
+}
+
+
+
